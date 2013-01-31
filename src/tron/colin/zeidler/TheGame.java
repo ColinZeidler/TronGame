3f62510ee15fae0449f09302e108a8172b7ae71f
ColinZeidler <c.zeidler.wc@gmail.com>
2013-01-30 21:43:00 -0500

    creating bauncher frame

--- src/tron/colin/zeidler/TheGame.java
+++ src/tron/colin/zeidler/TheGame.java
@@ -1,6 +1,20 @@
 package tron.colin.zeidler;
-public class TheGame{
+
+import javax.swing.JFrame;
+
+public class TheGame extends JFrame{
+	public TheGame(String string) {
+		super(string);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(400, 200);
+		setResizable(false);
+	}
+
 	public static void main(String[] args) {
+		TheGame frame;
+		
+		frame = new TheGame("Tron Launcher");
+		frame.setVisible(true);
 		new Window(500, 500);
 
 	}
