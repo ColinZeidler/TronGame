3d4199d22599c942b64499999b24bf91d6cb1c2e
ColinZeidler <c.zeidler.wc@gmail.com>
2013-01-30 22:37:06 -0500

    added button to first menu to launch the game

--- src/tron/colin/zeidler/TheGame.java
+++ src/tron/colin/zeidler/TheGame.java
@@ -1,22 +1,47 @@
 package tron.colin.zeidler;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class TheGame extends JFrame{
+public class TheGame extends JFrame implements ActionListener{
+	static TheGame frame;
 	public TheGame(String string) {
 		super(string);
+		
+		getContentPane().setLayout(null);
+		
+		JButton launch = new JButton("Launch Game");
+		launch.setSize(120, 30);
+		launch.setLocation(10, 10);
+		launch.addActionListener(this);
+		getContentPane().add(launch);
+		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(400, 200);
 		setResizable(false);
+		
 	}
 
 	public static void main(String[] args) {
-		TheGame frame;
 		
 		frame = new TheGame("Tron Launcher");
 		frame.setVisible(true);
-		new Window(500, 500);
+		
+		
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent action) {
+		String act = action.getActionCommand();
+		if (act == "Launch Game") {
+			new Window(500, 500);
+			frame.setVisible(false);
+		}
+		
 	}
 
 }
